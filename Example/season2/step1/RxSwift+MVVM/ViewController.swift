e262ac9465543fe6235c9fb7cd9c8cfc4f70b7e2
Mingu Kang <minqu.kang@gmail.com>
2021-03-22 18:25:57 +0900

    step1

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -15,14 +15,17 @@ let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
-
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
-
+    
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
         UIView.animate(withDuration: 0.3, animations: { [weak v] in
@@ -31,20 +34,30 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
+    
+    func downloadJson(_ url: String) -> Observable<String?> {
+        // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+        return Observable.from(url)
+    }
+    
     // MARK: SYNC
-
+    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
+    
     @IBAction func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        // 2. Observable로 오는 데이터를 받아서 처리하는 방법
+        downloadJson(MEMBER_LIST_URL)
+            .map { link in link?.count ?? 0}
+            .filter { count in count > 10}
+            .map { "\($0)"}
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: {
+                self.editView.text = $0
+                self.setVisibleWithAnimation(self.activityIndicator, false)
+            })
+            .disposed(by: disposeBag)
     }
 }
